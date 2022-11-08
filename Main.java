5761dc2a45f82de563814546d243868e4bd5c66a
菊池響 <h.kikuchi.sys22@morijyobi.ac.jp>
2022-11-08 10:23:59 +0900

    間違ったコミットメッセージ

--- Main.java
+++ Main.java
@@ -3,9 +3,13 @@ public class  Main {
     public static void main(String[] args) {
      Scanner scanner = new Scanner(System.in);
      double height = scanner.nextDouble();
+     double height2=scanner.nextDouble();
      double weight = scanner.nextDouble();
-    double bmi = weight*2 / (height * height)*2;
+     double weight2=scanner.nextDouble();
+    double bmi = weight / (height * height);
+    double bmi2=weight2 / (height2*height2);
 
       System.out.println(Math.round(bmi));
+      System.out.println(Math.round(bmi2));
      }
    }
